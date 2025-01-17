620b17fd5cdc0b00a7309ed599d8c07215c06e4c
esdras-sena <esantoz@protonmai.com>
2025-01-16 21:52:05 -0300

    text button bug fixed

--- typhoon-ui/app/context/StarknetProvider.jsx
+++ typhoon-ui/app/context/StarknetProvider.jsx
@@ -33,8 +33,8 @@ export function StarknetProvider({ children }) {
 
   // const apiKey = '5h7a0P2yMQsRFhIKvL0mlPur0zEAcViZ6E7OYMw1UxCe0kcGvnUVuOZsBURn4PdM';
   // const nodeProvider = 'nethermind';
-  const apiKeye = process.env.NEXT_PUBLIC_API_KEY
-  const nodeProvidere = process.env.NEXT_PUBLIC_PROVIDER
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY
+  const nodeProvider = process.env.NEXT_PUBLIC_PROVIDER
 
   let provider;
   if (nodeProvider == "infura") {
